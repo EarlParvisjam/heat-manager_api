012686361e54cb405a154ef7de936c0fd3359cc3
antstr <earl_parvisjam@yahoo.com>
2026-05-19 21:21:38 -0500

    Initial commit

---
+++ src/main/java/com/parvisjam/heat_manager/HeatManagerApplication.java
@@ -0,0 +1,13 @@
+package com.parvisjam.heat_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parvisjam/heat_manager/HeatManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parvisjam.heat_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
